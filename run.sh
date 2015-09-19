eeea6515999d8bc90b167b4b3a30ed8b4228a5df
Andreas Kruger <ak@krugercorp.dk>
2015-09-19 17:50:00 +0200

    Lame bug

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This runs INSIDE the docker container.
 PATH_KEEPALIVED_CONF="/etc/keepalived/keepalived.conf"
+
 interface=${KEEPALIVED_INTERFACE:-eth0}
 priority=${KEEPALIVED_PRIORITY:-100}
 peer=${KEEPALIVED_PEER:-172.16.50.183}
@@ -15,4 +16,4 @@ perl -p -i -e "s/\{\{ floating_ip \}\}/$floating_ip/" $PATH_KEEPALIVED_CONF
 perl -p -i -e "s/\{\{ password \}\}/$password/" $PATH_KEEPALIVED_CONF
 
 # Foreground keepalived
-/usr/sbin/keepalived -f /etc/keepalived/keepalived.conf --dont-fork --log-console"
+/usr/sbin/keepalived -f /etc/keepalived/keepalived.conf --dont-fork --log-console
